6c52c2df53a0a799ccc9097e24785b3fcb07d5ff
danhammer <dan.s.hammer@gmail.com>
2010-12-11 13:37:00 -0500

    adjusted format_grid.py

--- format_grid.py
+++ format_grid.py
@@ -1,5 +1,13 @@
+import os
+import numpy as np
+
+os.chdir('/Users/danhammer/Desktop/workspace/code/worldbank/')
 
 width = 4320
 height = 2160
 
+
 print width
+
+
+data = np.load('rff_oct4.npy.gz')
